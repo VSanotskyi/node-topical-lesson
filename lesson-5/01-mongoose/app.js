255cc096aea1472d1dbbc1998695dc68653c1e50
Vsanotskyi <vsanotskyi@gmail.com>
2024-02-21 14:31:46 +0200

    lesson 5

---
+++ lesson-5/01-mongoose/app.js
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import mongoose from "mongoose";
+
+const DB_URI = process.env.DB_URI;
+
+mongoose.connect(DB_URI)
+    .then(() => {
+        console.log("Database connection success");
+        process.exit(0);
+    })
+    .catch(error => {
+        console.error("Database connection error", error);
+        process.exit(1);
+    });
