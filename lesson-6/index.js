79fda72128dd950fa6dec89c4e97f2afd0692411
Vsanotskyi <vsanotskyi@gmail.com>
2024-02-23 14:59:48 +0200

    lesson 6

---
+++ lesson-6/index.js
@@ -0,0 +1,24 @@
+import "dotenv/config";
+
+import express from "express";
+
+import routes from "./routes/index.js";
+
+import "./db.js";
+
+const app = express();
+
+app.use("/api", routes);
+
+app.use((req, res) => {
+    res.status(404).send("Not found");
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+});
+
+app.listen(8080, () => {
+    console.log("Server started on port 8080");
+});
